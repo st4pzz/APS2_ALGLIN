80c4dd2bf4ef6ec8ae3ba1d3803d9578c0460b87
st4pzz <ramellajr@gmail.com>
2023-03-02 17:25:50 -0300

    cifrar teste + demo

--- demo.py
+++ demo.py
@@ -1,11 +1,15 @@
-from funcoes import para_one_hot,para_string
+from funcoes import para_one_hot,para_string,cifrar
 import numpy as np
 
 
 msg = 'abc'
 one_hot = para_one_hot(msg)
-print(one_hot)
+# print(one_hot)
 
-msg = np.array([[[1],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[1],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[1],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]]])
-one_hot = para_string(msg)
-print(one_hot)
+
+msg2 = para_string(one_hot)
+# print(msg2)
+
+P = np.array([[0],[1],[0]])
+ms3 = cifrar(msg,P)
+print(ms3)
--- funcoes.py
+++ funcoes.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def para_one_hot(msg: str):
-    alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+    alfabeto = 'abcdefghijklmnopqrstuvwxyz '
     msg = msg.lower()
     one_hot = np.zeros((len(msg),len(alfabeto)))
     for i in range(len(msg)):
@@ -10,15 +10,20 @@ def para_one_hot(msg: str):
     return one_hot.T  
     
 def para_string(M : np.array):
-    alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+    alfabeto = 'abcdefghijklmnopqrstuvwxyz '
     msg = ''
-    for i in range(M.shape[0]):
-        msg += alfabeto[np.argmax(M[:,i])]
+    for linha in M:
+        for i in range(len(linha)):
+            if linha[i] == 1:
+                msg += alfabeto[i]
     return msg
 
     
 def cifrar(msg: str,P : np.array):
-    return
+    msg_one_hot = para_one_hot(msg)
+    msg_cifrada = msg_one_hot@P
+    return para_string(msg_cifrada)
+    
 def de_cifrar(msg: str,P : np.array):
     return
 def enigma(msg: str, P : np.array, E : np.array):
