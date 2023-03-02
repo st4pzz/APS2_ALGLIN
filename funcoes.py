f7e9368faaaa31df9ef5121fd0f4c77c84daca33
st4pzz <ramellajr@gmail.com>
2023-03-02 16:09:49 -0300

    commit infra

---
+++ funcoes.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def para_one_hot(msg: str):
+    return
+def para_string(M : np.array):
+    return
+def cifrar(msg: str,P : np.array):
+    return
+def de_cifrar(msg: str,P : np.array):
+    return
+def enigma(msg: str, P : np.array, E : np.array):
+    return
+def de_enigma(msg: str, P : np.array, E : np.array):
+    return
