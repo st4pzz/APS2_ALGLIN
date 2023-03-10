96e7186203c94d20dae6098597523559fef520da
st4pzz <ramellajr@gmail.com>
2023-03-10 11:48:52 -0300

    notebook teste api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from enigma import enigma,de_enigma
 import numpy as np
+
 
 app = Flask(__name__)
 
@@ -12,22 +13,20 @@ segundo_encoder = np.roll(alfabeto_identidade,7,1)
 #Rota que recebe um json com a mensagem e retorna a mensagem cifrada
 @app.route("/post/enigma", methods=["POST"])
 def enigma1():
-    
-    if request.method == "POST":
-        msg = request.get_json()
-        msg = msg["mensagem"]
-        msg_enigma = enigma(msg,encoder,segundo_encoder)
-        return f"""<h1>A sua mensagem cifrada é : ( {msg_enigma})  <h1>"""
+    msg = request.get_json()
+    msg = msg["mensagem"]
+    msg_enigma = enigma(msg,encoder,segundo_encoder)
+    return jsonify({"mensagem": msg_enigma})
 
 
 #Rota que recebe um json com a mensagem cifrada e retorna a mensagem decifrada
 @app.route("/post/de-enigma", methods=["POST"])
 def de_enigma1():
-    if request.method == "POST":
-        msg = request.get_json()
-        msg = msg["mensagem"]
-        msg_de_enigma = de_enigma(msg,encoder,segundo_encoder)
-        return f"""A sua mensagem decifrada é : ( {msg_de_enigma}) <h1>"""
+
+    msg = request.get_json()
+    msg = msg["mensagem"]
+    msg_de_enigma = de_enigma(msg,encoder,segundo_encoder)
+    return jsonify({"mensagem": msg_de_enigma})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 from enigma import para_one_hot,para_string,cifrar, de_cifrar, enigma, de_enigma
 import numpy as np
 from colorama import init, Fore, Style
+from app import app
+
 
 #implemntando alfebeto e encoder
 alfabeto = 'abcdefghijklmnopqrstuvwxyz ãé,.'
@@ -8,72 +10,74 @@ alfabeto_identidade = np.identity(len(alfabeto))
 encoder = np.roll(alfabeto_identidade,-2,1)
 segundo_encoder = np.roll(alfabeto_identidade,7,1)
 
-print()
-print(Fore.RED + "FUNCOES: para_one_hot, para_string, cifrar, de_cifrar")
-print("-----------------------------------------------------"+ Style.RESET_ALL)
-print()
-print("-> testando one_hot, para_string, cifrar e de_cifrar com a mensagem 'eu sou o goku'")
-print()
+# print()
+# print(Fore.RED + "FUNCOES: para_one_hot, para_string, cifrar, de_cifrar")
+# print("-----------------------------------------------------"+ Style.RESET_ALL)
+# print()
+# print("-> testando one_hot, para_string, cifrar e de_cifrar com a mensagem 'eu sou o goku'")
+# print()
 
-#one_hot
-msg = 'eu sou o goku'
-one_hot = para_one_hot(msg)
-print(Fore.BLUE + "one_hot:" + Style.RESET_ALL)
-print()
-print(one_hot)
-print()
+# #one_hot
+# msg = 'eu sou o goku'
+# one_hot = para_one_hot(msg)
+# print(Fore.BLUE + "one_hot:" + Style.RESET_ALL)
+# print()
+# print(one_hot)
+# print()
 
-msg = "eu sou o goku"
-one_hot = para_one_hot(msg)
-msg = para_string(one_hot)
-print(Fore.BLUE + "para_string:" + Style.RESET_ALL)
-print()
-print(msg)
-print()
+# msg = "eu sou o goku"
+# one_hot = para_one_hot(msg)
+# msg = para_string(one_hot)
+# print(Fore.BLUE + "para_string:" + Style.RESET_ALL)
+# print()
+# print(msg)
+# print()
 
-# cifrar
-msg = "eu sou o goku"
-one_hot = para_one_hot(msg)
-msg_cifrada = cifrar(msg,encoder)
-print(Fore.BLUE + "cifrar:" + Style.RESET_ALL)
-print()
-print(msg_cifrada)
-print()
+# # cifrar
+# msg = "eu sou o goku"
+# one_hot = para_one_hot(msg)
+# msg_cifrada = cifrar(msg,encoder)
+# print(Fore.BLUE + "cifrar:" + Style.RESET_ALL)
+# print()
+# print(msg_cifrada)
+# print()
 
-#de_cifrar
-msg = "eu sou o goku"
-one_hot = para_one_hot(msg)
-msg_cifrada = cifrar(msg,encoder)
-msg_decifrada = de_cifrar(msg_cifrada,encoder)
-print(Fore.BLUE + "de_cifrar:" + Style.RESET_ALL)
-print()
-print(msg_decifrada)
-print()
+# #de_cifrar
+# msg = "eu sou o goku"
+# one_hot = para_one_hot(msg)
+# msg_cifrada = cifrar(msg,encoder)
+# msg_decifrada = de_cifrar(msg_cifrada,encoder)
+# print(Fore.BLUE + "de_cifrar:" + Style.RESET_ALL)
+# print()
+# print(msg_decifrada)
+# print()
 
-print()
-print(Fore.RED + "FUNCOES: enigma, de_enigma" + Style.RESET_ALL)
-print(Fore.RED +"-----------------------------------------------------" + Style.RESET_ALL)
-print()
-print("-> testando enigma e de_enigma com a mensagem 'eu estou com fome'")
-print()
+# print()
+# print(Fore.RED + "FUNCOES: enigma, de_enigma" + Style.RESET_ALL)
+# print(Fore.RED +"-----------------------------------------------------" + Style.RESET_ALL)
+# print()
+# print("-> testando enigma e de_enigma com a mensagem 'eu estou com fome'")
+# print()
 
-#enigma
-msg = "eu estou com fome"
-one_hot = para_one_hot(msg)
-msg_cifrada = cifrar(msg,encoder)
-msg_enigma = enigma(msg,encoder,segundo_encoder)
-print(Fore.BLUE + "enigma:" + Style.RESET_ALL)
-print()
-print(msg_enigma)
-print()
+# #enigma
+# msg = "eu estou com fome"
+# one_hot = para_one_hot(msg)
+# msg_cifrada = cifrar(msg,encoder)
+# msg_enigma = enigma(msg,encoder,segundo_encoder)
+# print(Fore.BLUE + "enigma:" + Style.RESET_ALL)
+# print()
+# print(msg_enigma)
+# print()
 
-#de_enigma
-msg = "eu estou com fome"
-one_hot = para_one_hot(msg)
-msg_cifrada = cifrar(msg,encoder)
-msg_enigma = enigma(msg,encoder,segundo_encoder)
-msg_de_enigma = de_enigma(msg_enigma,encoder,segundo_encoder)
-print(Fore.BLUE + "de_enigma:" + Style.RESET_ALL)
-print()
-print(msg_de_enigma)
-print()
+# #de_enigma
+# msg = "eu estou com fome"
+# one_hot = para_one_hot(msg)
+# msg_cifrada = cifrar(msg,encoder)
+# msg_enigma = enigma(msg,encoder,segundo_encoder)
+# msg_de_enigma = de_enigma(msg_enigma,encoder,segundo_encoder)
+# print(Fore.BLUE + "de_enigma:" + Style.RESET_ALL)
+# print()
+# print(msg_de_enigma)
+# print()
+
+
